7f9ccd7e579c8d6edbdb993f7301eb5a58a93e3e
kmorgport <kmorgan0057@gmail.com>
2021-07-29 16:35:09 -0500

    useEffect and useReduce implemented

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,34 @@
-import {useContext} from 'react'
+import {useContext, useEffect, useState} from 'react'
 import CartIcon from '../Cart/CartIcon'
 import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css'
 
 
 const HeaderCartButton = props => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false)
     const cartCtx = useContext(CartContext)
     const numberOfCartItems = cartCtx.items.reduce((currNum, item)=>{
         return currNum + item.amount
     },0)
+
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+    const { items } = cartCtx
+
+    useEffect(()=>{
+        if(items.length === 0){
+            return
+        }
+        setBtnIsHighlighted(true)
+        const timer = setTimeout(()=> {
+            setBtnIsHighlighted(false)
+        },300)
+        return () =>{
+            clearTimeout(timer)
+        }
+    }, [items])
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon/>
             </span>
